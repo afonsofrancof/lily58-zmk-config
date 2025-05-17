1a7fff21699b6a46bd9557a80d18adf39ac253a0
afonsofrancof <afonso@francof.net>
2025-05-17 15:05:42 +0100

    Added homerow mods

---
+++ config/lily58_positions.h
@@ -0,0 +1,80 @@
+#pragma once
+
+#define LT0  5
+#define LT1  4
+#define LT2  3
+#define LT3  2
+#define LT4  1
+#define LT5  0
+
+#define RT0  6
+#define RT1  7
+#define RT2  8
+#define RT3  9
+#define RT4 10
+#define RT5 11
+
+#define LN0 17
+#define LN1 16
+#define LN2 15
+#define LN3 14
+#define LN4 13
+#define LN5 12
+
+#define RN0 18
+#define RN1 19
+#define RN2 20
+#define RN3 21
+#define RN4 22
+#define RN5 23
+
+#define LM0 29
+#define LM1 28
+#define LM2 27
+#define LM3 26
+#define LM4 25
+#define LM5 24
+
+#define RM0 30
+#define RM1 31
+#define RM2 32
+#define RM3 33
+#define RM4 34
+#define RM5 35
+
+#define LB0 41
+#define LB1 40
+#define LB2 39
+#define LB3 38
+#define LB4 37
+#define LB5 36
+
+#define LBC 42
+#define RBC 43
+
+#define RB0 44
+#define RB1 45
+#define RB2 46
+#define RB3 47
+#define RB4 48
+#define RB5 49
+
+#define LF0 53
+#define LF1 52
+#define LF2 51
+#define LF3 50
+
+#define RF0 54
+#define RF1 55
+#define RF2 56
+#define RF3 57
+
+#define KEYS_L LT0 LT1 LT2 LT3 LT4 LT5 LN0 LN1 LN2 LN3 LN4 LN5 LM0 LM1 LM2 LM3 LM4 LM5 LBC LB0 LB1 LB2 LB3 LB4 LB5
+
+#define KEYS_R RT0 RT1 RT2 RT3 RT4 RT5 RN0 RN1 RN2 RN3 RN4 RN5 RM0 RM1 RM2 RM3 RM4 RM5 RBC RB0 RB1 RB2 RB3 RB4 RB5
+
+#define THUMB_L LF3 LF2 LF1 LF0
+
+#define THUMB_R RF0 RF1 RF2 RF3
+
+#define THUMBS THUMB_L THUMB_R
